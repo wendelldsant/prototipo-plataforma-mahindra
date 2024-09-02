89eacbb100c8a995e6c935f0a1b589fe37ba3c00
wendelldsant <wendell_dsantos@hotmail.com>
2024-09-02 20:58:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# prototipo-plataforma-mahindra
+ Conteúdo do Repositorio: Prototipo funcional do projeto idealizado para a challenge 2024  proposto pela FIAP e Tech Mahindra.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from "./Componentes/Header"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
