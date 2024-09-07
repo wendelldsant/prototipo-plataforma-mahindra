d1ba7a8acb03e4a58df2398c31b2b7935826fa58
Wendell dos Santos <wendell_dsantos@hotmail.com>
2024-09-06 21:24:53 -0300

    add-Button Function Test

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,33 @@
 function Register() {
+    const handleCadastreSe = () => {
+        const newDiv = document.createElement('div');
+        newDiv.textContent = 'Novo elemento criado!';
+        
+        const container = document.querySelector('.text-xl');
+        
+        if (container) {
+            container.appendChild(newDiv);
+        }
+    };
+
     return ( 
-        <h1>Register Page</h1>
-     );
+        <div className="container">
+            <section className="text-xl">
+                <div>
+                    <button type="button" id="btn-login">Login</button>
+                </div>
+                <div className="button-container">
+                    <button type="button" id="btnCadastreSe" onClick={handleCadastreSe}>Cadastre-se</button>
+                </div>
+                <div className="button-container">
+                    <button type="submit" id="btn-cadastrar">Cadastrar</button>
+                </div>
+                <div className="button-container">
+                    <button type="button" id="btn-voltar" className="hidden">Voltar</button>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default Register;
