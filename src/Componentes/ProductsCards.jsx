2046f464b6abb1d5c209fa3373b4c3fb8a08e421
wendelldsant <wendell_dsantos@hotmail.com>
2024-09-25 14:10:41 -0300

    feat: Add Modal on Buy Button

--- src/Componentes/ProductsCards.jsx
+++ src/Componentes/ProductsCards.jsx
@@ -1,18 +1,87 @@
+import StandardModal from "./StandardModal";
+import React, { useState } from "react";
+
 function ProductsCard({ dados }) {
+
+  const [showPurchase, setShowPurchase] = useState(false)
+
+
+  const handlePurchaseConfirm = () => {
+    setShowPurchase(true);
+
+  }
+
+  const handleCloseModal = () => {
+    setShowPurchase(false);
+  }
+
   return (
-      <div className="grid grid-cols-4 gap-2">
-          {dados.map((pegaDados, index) => (
-              <div key={index} className="max-w-sm bg-white hover:shadow-lg rounded-lg overflow-hidden flex flex-col items-center ">
-                  <h1 className="text-lg font-bold mb-2">{pegaDados.titulo}</h1>
-                  <img src={pegaDados.imagem} alt={pegaDados.titulo} className="w-full h-48 object-contain" />
-                  <div className="flex gap-2 justify-center items-center m-2">
-                    <h1 className=" text-gray-700">{pegaDados.preco}</h1>
-                    <button className="bg-orange-400 rounded-lg text-color-white">COMPRAR</button>
-                  </div>
+    <div>
+      {showPurchase && <StandardModal handleCloseModal={handleCloseModal} />}  {/* Exibe o Modal ao clicar */}
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-4 w-auto">
+        {dados.map((pegaDados, index) => (
+          <div
+            key={index}
+            className="col-span-1 w-auto p-4 bg-white rounded-lg shadow-md transform hover:scale-105 transition-transform duration-300 ease-in-out overflow-hidden"
+          >
+            <div>
+              <img
+                className="w-full h-40 object-contain rounded-t-lg flex flex-col items-center "
+                alt={pegaDados.titulo}
+                src={pegaDados.imagem}
+              />
+              <div className="p-4">
+                <h2 className="text-xl text-center  font-semibold">{pegaDados.titulo}</h2>
+                <p className="text-gray-600 text-center">
+                  {pegaDados.preco}
+                </p>
+                <div className="flex justify-center gap-3 items-center mt-4">
+                  <button
+                    className="bg-blue-500 
+                 text-white px-4 py-2 rounded-full 
+                   border-solid border-2  border-blue-600
+                 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 hover:scale-110"
+                    onClick={handlePurchaseConfirm}
+                  >
+                    Comprar
+                  </button>
+                  <button
+                    className="bg-transparent
+                  text-blue-600 px-4 py-2 
+                    border-solid border-2 border-blue-600 rounded-full 
+                  hover:bg-blue-600 hover:text-white 
+                    focus:ring-2 focus:ring-blue-400 hover:scale-110"
+                  >
+                    
+                    Detalhes
+                  </button>
+
+                </div>
               </div>
-          ))}
+            </div>
+          </div>
+
+
+        ))}
+
       </div>
+    </div>
+
   );
 }
 
 export default ProductsCard;
+
+
+{/* <div className="grid grid-cols-4 gap-2">
+{dados.map((pegaDados, index) => (
+    <div key={index} className="max-w-sm bg-white hover:shadow-lg rounded-lg overflow-hidden flex flex-col items-center ">
+        <h1 className="text-lg font-bold mb-2">{pegaDados.titulo}</h1>
+        <img src={pegaDados.imagem} alt={pegaDados.titulo} className="w-full h-48 object-contain" />
+        <div className="flex gap-2 justify-center items-center m-2">
+          <h1 className=" text-gray-700">{pegaDados.preco}</h1>
+          <button className="bg-orange-400 rounded-lg text-color-white">COMPRAR</button>
+        </div>
+    </div>
+))}
+</div> */}
---
+++ src/Componentes/StandardModal.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+
+function StandardModal({handleCloseModal}) {
+
+
+  return (
+    // Overlay cobrindo toda a tela
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      {/* Modal */}
+      <div className="max-w-xl w-full mx-auto bg-gray-900 rounded-xl overflow-hidden z-10">
+        <div className="max-w-md mx-auto pt-12 pb-14 px-5 text-center">
+          <div className="inline-flex items-center justify-center w-12 h-12 mb-5 rounded-full">
+            <svg
+              viewBox="0 0 48 48"
+              height="100"
+              width="100"
+              y="0px"
+              x="0px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <linearGradient
+                gradientUnits="userSpaceOnUse"
+                y2="37.081"
+                y1="10.918"
+                x2="10.918"
+                x1="37.081"
+                id="SVGID_1__8tZkVc2cOjdg_gr1"
+              >
+                <stop stopColor="#60fea4" offset="0" />
+                <stop stopColor="#6afeaa" offset=".033" />
+                <stop stopColor="#97fec4" offset=".197" />
+                <stop stopColor="#bdffd9" offset=".362" />
+                <stop stopColor="#daffea" offset=".525" />
+                <stop stopColor="#eefff5" offset=".687" />
+                <stop stopColor="#fbfffd" offset=".846" />
+                <stop stopColor="#fff" offset="1" />
+              </linearGradient>
+              <circle
+                fill="url(#SVGID_1__8tZkVc2cOjdg_gr1)"
+                r="18.5"
+                cy="24"
+                cx="24"
+              />
+              <path
+                d="M35.401,38.773C32.248,41.21,28.293,42.66,24,42.66C13.695,42.66,5.34,34.305,5.34,24\tc0-2.648,0.551-5.167,1.546-7.448"
+                strokeWidth={3}
+                strokeMiterlimit={10}
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                stroke="#10e36c"
+                fill="none"
+              />
+              <path
+                d="M12.077,9.646C15.31,6.957,19.466,5.34,24,5.34c10.305,0,18.66,8.354,18.66,18.66\tc0,2.309-0.419,4.52-1.186,6.561"
+                strokeWidth={3}
+                strokeMiterlimit={10}
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                stroke="#10e36c"
+                fill="none"
+              />
+              <polyline
+                points="16.5,23.5 21.5,28.5 32,18"
+                strokeWidth={3}
+                strokeMiterlimit={10}
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                stroke="#10e36c"
+                fill="none"
+              />
+            </svg>
+          </div>
+          <h4 className="text-xl text-gray-100 font-semibold mb-5">
+            Your account is activated!
+          </h4>
+          <p className="text-gray-300 font-medium">
+            Account has been verified Successfully.
+          </p>
+        </div>
+        <div className="pt-5 pb-6 px-6 text-right bg-gray-800 -mb-2">
+          <button
+            onClick={handleCloseModal}
+            className="inline-block w-full sm:w-auto py-3 px-5 mb-2 mr-4 text-center font-semibold leading-6 text-gray-200 bg-gray-500 hover:bg-gray-400 rounded-lg transition duration-200"
+          >
+            Cancel
+          </button>
+          <button className="inline-block w-full sm:w-auto py-3 px-5 mb-2 text-center font-semibold leading-6 text-blue-50 bg-green-500 hover:bg-green-600 rounded-lg transition duration-200">
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StandardModal;
