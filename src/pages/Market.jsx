814a4c659bdcf5a1e348a718dc997919531c9698
wendelldsant <wendell_dsantos@hotmail.com>
2024-09-25 14:11:15 -0300

    perf: Transfer Products data to Json

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,84 +1,12 @@
 import ProductsCard from "../Componentes/ProductsCards.jsx";
-import camisa from "../images/camisa-red-bull.webp";
-import carro from "../images/carro.jpg"
-
-const products = [
-    {
-        titulo: 'Camisa Redbull',
-        imagem: camisa,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Camisa Redbull',
-        imagem: camisa,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Camisa Redbull',
-        imagem: camisa,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Camisa Redbull',
-        imagem: camisa,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Camisa Redbull',
-        imagem: camisa,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-    {
-        titulo: 'Carro',
-        imagem: carro,
-        preco: '10xp'
-    },
-];
+import data from '../dados/produtos.json';
 
 function Market() {
     return ( 
         <main className="m-5 p-4 grid grid-cols-5">
-                <article className="row-start-2">
-                    <h1>lateral</h1>
-                </article>
-                <h1 className="col-span-4 col-start-2 text-center">Marketplace</h1>
-                <section className="col-span-4 row-start-2 col-start-2 my-2">
-                    <ProductsCard dados={products} />
+                <h1 className="col-span-5 text-3xl text-center">Marketplace</h1>
+                <section className="col-span-6 my-2">
+                    <ProductsCard dados={data} />
                 </section>
         </main>
     );
