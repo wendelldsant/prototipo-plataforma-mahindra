a83fa8f014237a0e6afea2859e94d7a2cadc5cbf
wendelldsant <wendell_dsantos@hotmail.com>
2024-09-28 19:23:27 -0300

    style: Change standar button style

--- src/Componentes/Buttons.jsx
+++ src/Componentes/Buttons.jsx
@@ -3,10 +3,13 @@ function Buttons({ dados }) {
         <>
             {
                 dados.map((pegaDados, index) => (
-                    <div className={pegaDados.style} key={pegaDados.id || index}>
-                        <button type="submit" id={pegaDados.id} onClick={pegaDados.action}>
-                            {pegaDados.name}
-                        </button>                        
+                    <div className = {pegaDados.divStyle}>
+                        <button
+                            className="col-span-2 flex justify-center gap-2 items-center mx-auto shadow-xl text-xl bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+                            onClick={pegaDados.handle}
+                        >
+                           {pegaDados.title}
+                        </button>                    
                     </div>
                 ))
             }
