67801b5ad2ac11681e7dddb110644b9709b908b6
wendelldsant <wendell_dsantos@hotmail.com>
2024-09-30 12:46:16 -0300

    styles: Add icons and navbar

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,30 +1,128 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { CgProfile } from "react-icons/cg";
+import { GiCheckeredFlag } from "react-icons/gi"; // Importa o ícone da bandeira
 
 function Header() {
-    return ( 
-        <header className="text-lg flex items-center min-h-16 justify-start bg-emerald-500 w-full p-3 font-medium">
-            <h1>Logo</h1>
-            <nav className="w-full mr-2.5">
-                <ul className="flex justify-end gap-8 ">
-                    <li>
-                        <Link to= '/live'>Live</Link>
-                    </li>
-                    <li>
-                        <Link to= '/social'>Social</Link>
-                    </li>
-                    <li>
-                        <Link to= '/market'>Market</Link>
-                    </li>
-                    <li>
-                        <Link to= '/profile'>Profile</Link>
-                    </li>
-                    <li>
-                        <Link to= '/register'>Register</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-     );
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return (
+        <div className="bg-gray-900">
+            <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+                <div className="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
+                    <ul className="flex items-center hidden space-x-8 lg:flex">
+                        <li className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+                            <Link to="/live">Live</Link>
+                        </li>
+                        <li className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+                            <Link to="/market">Marketplace</Link>
+                        </li>
+                    </ul>
+                    <Link to="/" aria-label="E-Motion Grid" title="E-Motion Grid" className="inline-flex items-center lg:mx-auto">
+                        <GiCheckeredFlag className="w-8 h-8 text-white" /> {/* Ícone da bandeira */}
+                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                            E-Motion Grid
+                        </span>
+                    </Link>
+                    <ul className="flex items-center hidden ml-auto space-x-8 lg:flex">
+                        <li className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+                            <Link to="/register">Faça Login ou Cadastre-se já!</Link>
+                        </li>
+                        <li className="font-medium tracking-wide text-xl text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+                            <Link to="/profile">
+                                <CgProfile />
+                            </Link>
+                        </li>
+                    </ul>
+                    <div className="ml-auto lg:hidden">
+                        <button
+                            aria-label="Open Menu"
+                            title="Open Menu"
+                            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                            onClick={() => setIsMenuOpen(true)}
+                        >
+                            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                                <path
+                                    fill="currentColor"
+                                    d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                                />
+                                <path
+                                    fill="currentColor"
+                                    d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                                />
+                                <path
+                                    fill="currentColor"
+                                    d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                                />
+                            </svg>
+                        </button>
+                        {isMenuOpen && (
+                            <div className="absolute top-0 left-0 w-full z-50">
+                                <div className="p-5 bg-white border rounded shadow-sm">
+                                    <div className="flex items-center justify-between mb-4">
+                                        <div>
+                                            <Link
+                                                to="/"
+                                                aria-label="E-Motion Grid"
+                                                title="E-Motion Grid"
+                                                className="inline-flex items-center"
+                                            >
+                                                <GiCheckeredFlag className="w-8 h-8 text-deep-purple-accent-400" /> {/* Ícone da bandeira */}
+                                                <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                                                    E-Motion Grid
+                                                </span>
+                                            </Link>
+                                        </div>
+                                        <div>
+                                            <button
+                                                aria-label="Close Menu"
+                                                title="Close Menu"
+                                                className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                onClick={() => setIsMenuOpen(false)}
+                                            >
+                                                <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 
+                                                        c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 
+                                                        c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <nav>
+                                        <ul className="space-y-4">
+                                            <li className="font-medium tracking-wide text-blue-900 transition-colors duration-200 hover:text-teal-accent-400">
+                                                <Link to="/">Home</Link>
+                                            </li>
+                                            <li className="font-medium tracking-wide text-blue-900 transition-colors duration-200 hover:text-teal-accent-400">
+                                                <Link to="/live">Live</Link>
+                                            </li>
+                                            <li className="font-medium tracking-wide text-blue-900 transition-colors duration-200 hover:text-teal-accent-400">
+                                                <Link to="/market">Marketplace</Link>
+                                            </li>
+                                            <li className="font-medium tracking-wide text-blue-900 transition-colors duration-200 hover:text-teal-accent-400">
+                                                <Link to="/register">Faça Login ou Cadastre-se já!</Link>
+                                            </li>
+                                            <li className="font-medium tracking-wide text-xl text-blue-900 transition-colors duration-200 hover:text-teal-accent-400">
+                                                <Link to="/profile">
+                                                    <CgProfile />
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
